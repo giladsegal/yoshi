94f57fd062f3156f07f7063f8e75491cc8c863b0
Dean Shub <deanshub@gmail.com>
2019-12-23 12:08:44 +0200

    wip

--- packages/yoshi-flow-editor/src/buildEditorEntires.ts
+++ packages/yoshi-flow-editor/src/buildEditorEntires.ts
@@ -1,45 +1,17 @@
 import path from 'path';
-import globby from 'globby';
 import componentWrapping from './componentWrapping';
 import editorAppWrapping from './editorAppWrapping';
 import settingsWrapping from './settingsWrapping';
 import viewerScriptWrapping from './viewerScriptWrapping';
 import wixPrivateMockWrapping from './wixPrivateMockWrapping';
+import { FlowEditorModel } from './model';
 
 const generatedWidgetEntriesPath = path.resolve(__dirname, '../tmp/components');
 
-export const buildEditorPlatformEntries = () => {
-  const userComponents = globby.sync('./src/components/*/Component.js', {
-    absolute: true,
-  });
-
-  const componentEntries = componentWrapping(
-    generatedWidgetEntriesPath,
-    userComponents,
-  );
-
-  const userControllers = globby.sync('./src/components/*/controller.js', {
-    absolute: true,
-  });
-  const userInitApp = globby.sync('./src/components/initApp.js', {
-    absolute: true,
-  });
-
-  const editorAppEntries = editorAppWrapping(
-    generatedWidgetEntriesPath,
-    userComponents,
-    userControllers,
-    userInitApp[0],
-  );
-
-  const userSettings = globby.sync('./src/components/*/Settings.js', {
-    absolute: true,
-  });
-
-  const settingsEntries = settingsWrapping(
-    generatedWidgetEntriesPath,
-    userSettings,
-  );
+export const buildEditorPlatformEntries = (model: FlowEditorModel) => {
+  const componentEntries = componentWrapping(generatedWidgetEntriesPath, model);
+  const editorAppEntries = editorAppWrapping(generatedWidgetEntriesPath, model);
+  const settingsEntries = settingsWrapping(generatedWidgetEntriesPath, model);
 
   const wixPrivateMockEntry = wixPrivateMockWrapping();
 
@@ -51,19 +23,8 @@ export const buildEditorPlatformEntries = () => {
   };
 };
 
-export const buildViewerScriptEntry = () => {
-  const userController = globby.sync('./src/components/*/controller.js', {
-    absolute: true,
-  });
-  const userInitApp = globby.sync('./src/components/initApp.js', {
-    absolute: true,
-  });
-
-  return viewerScriptWrapping(
-    generatedWidgetEntriesPath,
-    userController,
-    userInitApp[0],
-  );
+export const buildViewerScriptEntry = (model: FlowEditorModel) => {
+  return viewerScriptWrapping(generatedWidgetEntriesPath, model);
 };
 
 export const webWorkerExternals = {
